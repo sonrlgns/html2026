7d626962834a9766f0f47b4a5d88568fd429ebb6
sonrlgns <dj20250609@dge.go.kr>
2026-03-10 15:15:07 +0900

    Add files via upload

---
+++ 260310.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int num;
+	
+	scanf("%d",&num);
+	
+	if(num >0)
+       printf("양수");
+	if else (num <0)
+	   printf("음수");
+	else 
+		printf("0");
+	return 0;
+} 
